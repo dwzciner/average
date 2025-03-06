8af0b492ca9e8d76e8cbd0d5a04f729869c7ce76
warma181 <113353512+dwzciner@users.noreply.github.com>
2025-03-06 10:13:54 +0800

    Add files via upload

---
+++ max.py
@@ -0,0 +1,27 @@
+import os
+
+filepath = input("enter filepath")
+test_result = "test_result.log"
+avg_test_result = "max_test_result.log"
+full_test_result = os.path.join(filepath, test_result)
+full_avg_test_result = os.path.join(filepath, avg_test_result)
+
+acc = []
+avg_acc = []
+with open(full_test_result, 'r') as file:
+    lines = file.readlines()
+    for line in lines:
+        acc.append(float(line.split()[-1]))
+
+acc10 = max(acc[:50])
+acc50 = max(acc[50 : 100])
+acc100 = max(acc[100:150])
+acc200 = max(acc[150:200])
+acc600 = max(acc[200:250])
+
+with open(full_avg_test_result, 'w') as file:
+    file.write(f"classes : 10  max test acc: {acc10}\n")
+    file.write(f"classes : 50  max test acc: {acc50}\n")
+    file.write(f"classes : 100  max test acc: {acc100}\n")
+    file.write(f"classes : 200  max test acc: {acc200}\n")
+    file.write(f"classes : 600  max test acc: {acc600}")
